3ecb5ce07032661100f6d39696e3eb416bd9f154
Jonas Lejeune <jonas.lejeune4420@gmail.com>
2023-11-19 17:34:48 +0900

    Update scripts directory structure

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ 1 -ne "$#" ]; then
+	echo "$0: missing destinaty directory" >&2
+	exit 1
+fi
+
+# Backup and delete extra files
+rsync -aPh --delete ${HOME}/Documents "$1"
+rsync -aPh --delete ${HOME}/Dropbox   "$1"
+rsync -aPh --delete ${HOME}/Music     "$1"
+rsync -aPh --delete ${HOME}/Pictures  "$1"
+rsync -aPh --delete ${HOME}/Workspace "$1"
--- hdd_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ 1 -ne "$#" ]; then
-	echo "$0: missing destinaty directory" >&2
-	exit 1
-fi
-
-rsync -aPh --delete ${HOME}/{Documents,Dropbox,Music,Pictures,Videos} $1
