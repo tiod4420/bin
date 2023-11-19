cdff1183a028237141d1d970e61d9aec25bdb478
Jonas Lejeune <jonas.lejeune4420@gmail.com>
2023-11-19 17:34:48 +0900

    Add script to backup Cryptomator

---
+++ cryptomator.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+safe=${HOME}/.local/share/Cryptomator/mnt/Safe/
+
+if [ ! -d "$safe" ]; then
+	echo "$safe: Cryptomator safe not mounted" >&2
+	exit 1
+fi
+
+# Backup and delete extra files
+rsync -aPh --delete ${HOME}/Documents/Backups   "$safe"
+rsync -aPh --delete ${HOME}/Documents/Documents "$safe"
